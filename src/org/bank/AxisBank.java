b9463cbf76f8a4e14c51e7d4b2ba53d9b5ec3b93
unknown <archana.skr@gmail.com>
2023-01-23 15:50:49 +0530

    changes

--- src/org/bank/AxisBank.java
+++ src/org/bank/AxisBank.java
@@ -5,17 +5,17 @@ public class AxisBank {
 		AxisBank b = new AxisBank();
 		b.saving();
 		
-<<<<<<< HEAD
+
 		AxisBank c = new AxisBank();
 		c.deposit();
 		
 		AxisBank cd = new AxisBank();
 		cd.deposit();
-=======
+
 		AxisBank d = new AxisBank();
 		d.deposit();
 
->>>>>>> 2dfc2d0f82974dd644d6c143124dffbb88a1ef8a
+
 }
 	public void deposit() { {System.out.println("1%");}	}
 }
